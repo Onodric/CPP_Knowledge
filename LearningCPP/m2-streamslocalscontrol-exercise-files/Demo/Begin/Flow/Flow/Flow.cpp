86d1172ad38913465cf7900c3cea67c87a117bfb
Onodric <onodric@yahoo.com>
2018-03-15 16:06:45 -0500

    Got through the first three modules

--- LearningCPP/m2-streamslocalscontrol-exercise-files/Demo/Begin/Flow/Flow/Flow.cpp
+++ LearningCPP/m2-streamslocalscontrol-exercise-files/Demo/Begin/Flow/Flow/Flow.cpp
@@ -5,29 +5,33 @@ using namespace std;
 int main()
 {
 	int i;
-	int j;
-	cout << "Enter a number: ";
-	cin >> i;
-	cout << "You entered " << i << ". Enter another number: ";
-	cin >> j;
+	int j = 7;
+	bool keepgoing = true;
 
-	if (i<j)
-	{
-		cout << "The first number, " << i << ", is less than the second number, " << j << endl;
-	}
-	else
-	{
-		cout << "The first number, " << i << ", is not less than the second number, " << j << endl;
-	}
-	if (i==j)
-	{
-		cout << "The first number, " << i << ", is equal to the second number, " << j << endl;
-	}
-	if (i>j)
-	{
-		cout << "The first number, " << i << ", is greater than the second number, " << j << endl;
+	while (keepgoing) {
+		cout << "Guess a number: ";
+		cin >> i;
+		cout << endl;
+
+		if (i < j)
+		{
+			cout << "Your guess, " << i << ", is less than my number..." << endl;
+		}
+		if (i > j)
+		{
+			cout << "Your guess, " << i << ", is greater than my number..." << endl;
+		}
+		if (i == j)
+		{
+			keepgoing = false;
+			cout << "Your guess, " << i << ", is equal to my number, " << j << endl;
+			for (int loop = 10; loop > 0; loop--)
+			{
+				cout << loop << endl;
+			}
+			cout << "Done!" << endl;
+		}
 	}
 
 	return 0;
 }
-
